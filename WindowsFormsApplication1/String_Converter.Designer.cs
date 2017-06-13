601fa056780c4fb0587286250321e6922353a656
jmcf91 <mcfarl61@gmail.com>
2017-06-12 22:14:37 -0400

    Initial commit
    String converter for Frontline

---
+++ WindowsFormsApplication1/String_Converter.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class String_Converter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.lstBxResults = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(177, 92);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(79, 28);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // lstBxResults
+            // 
+            this.lstBxResults.FormattingEnabled = true;
+            this.lstBxResults.Location = new System.Drawing.Point(126, 126);
+            this.lstBxResults.Name = "lstBxResults";
+            this.lstBxResults.Size = new System.Drawing.Size(179, 121);
+            this.lstBxResults.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "\"(id,created,employee(id,firstname,employeeType(id), lastname),location)\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Click Print to convert:";
+            // 
+            // String_Converter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 283);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBxResults);
+            this.Controls.Add(this.btnPrint);
+            this.Name = "String_Converter";
+            this.Text = "String Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.ListBox lstBxResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/String_Converter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class String_Converter : Form
+    {
+        //using list so I don't have to declare set number of elements
+        private List<string> formatList = new List<string>();
+
+        public String_Converter()
+        {
+            InitializeComponent();
+        }
+
+        //button click from Form
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            //original string
+            string strVariable = "(id,created,employee(id,firstname,employeeType(id), lastname),location)";
+
+            //removes parens and commas
+            string tempString = strVariable.Replace('(', ' ').Replace(')', ' ').Replace(',',' ');
+
+            //puts string items into list and removes blank spaces
+            formatList = tempString.Split(' ').Where(l => l != "").ToList();
+
+            //for loop to format and print string values
+            for (int i = 0; i < formatList.Count(); i++)
+            {
+                //first three list records and last
+                if (i < 3 || i == 8)
+                {
+                    lstBxResults.Items.Add(formatList[i]);
+                }
+                //for double dash record
+                else if (i == 6)
+                {
+                    lstBxResults.Items.Add("-- " + formatList[i]);
+                }
+                //for single dash records
+                else
+                {
+                    lstBxResults.Items.Add("- " + formatList[i]);
+                }
+            }
+            //OUTPUTS
+            //id
+            //created
+            //employee
+            //- id
+            //- firstname
+            //- employeeType
+            //-- id
+            //- lastname
+            //location
+        }
+    }
+}
